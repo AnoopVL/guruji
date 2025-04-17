eaff488044a533a943431db445be8bfe6345ba07
AnoopVL <avlanjekar4@gmail.com>
2025-04-17 12:05:38 +0530

    status bar added

--- app/(main)/dashboard/create-interview/page.jsx
+++ app/(main)/dashboard/create-interview/page.jsx
@@ -1,10 +1,12 @@
 "use client";
-import React from "react";
+import React, { useState } from "react";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
+import { Progress } from "@/components/ui/progress";
 
 function CreateInterview() {
   const router = useRouter();
+  const [step, setStep] = useState(1);
   return (
     <>
       <div className="mt-10 px-10 md:px-24 lg:px-px-44 xl:px-56">
@@ -12,6 +14,7 @@ function CreateInterview() {
           <ArrowLeft onClick={() => router.back()} className="cursor-pointer" />
           <h2 className="font-bold text-2xl">Create a new interview</h2>
         </div>
+        <Progress value={step * 33} className="my-5" />
       </div>
     </>
   );
