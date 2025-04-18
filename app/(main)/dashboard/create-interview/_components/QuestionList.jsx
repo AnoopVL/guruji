f1b104c407fa900fc4c7e0deee6529653e526289
AnoopVL <avlanjekar4@gmail.com>
2025-04-18 19:39:00 +0530

    question list api calls + loading component

--- app/(main)/dashboard/create-interview/_components/QuestionList.jsx
+++ app/(main)/dashboard/create-interview/_components/QuestionList.jsx
@@ -1,8 +1,49 @@
 // app/(main)/dashboard/create-interview/_components/QuestionList.jsx
-import React from "react";
+import axios from "axios";
+import React, { useEffect } from "react";
+import { toast } from "sonner";
+import { LoaderCircle } from "lucide-react";
+import { useState } from "react";
 
 function QuestionList({ formData }) {
-  return <div>QuestionList</div>;
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    if (formData) {
+      GenerateQuestionList();
+    }
+  }, [formData]);
+  const GenerateQuestionList = async () => {
+    setLoading(true);
+    try {
+      const result = await axios.post("/api/ai-model", {
+        ...formData,
+      });
+      console.log(result.data);
+      setLoading(false);
+    } catch (error) {
+      console.log(error);
+      toast("Server error, try again after some time");
+      setLoading(false);
+    }
+  };
+  return (
+    <>
+      <div>
+        {loading && (
+          <div className="p-5 bg-green-100 rounded-xl border border-primary shadow-xl flex gap-5 items-center">
+            <LoaderCircle className="animate-spin" />
+            <div>
+              <h2 className="font-medium">Generation Interview questions</h2>
+              <p className="text-primary">
+                Our Ai is crafting personalized interview questions for your job
+                description
+              </p>
+            </div>
+          </div>
+        )}
+      </div>
+    </>
+  );
 }
 
 export default QuestionList;
