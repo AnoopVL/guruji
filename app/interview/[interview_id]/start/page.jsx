bb0a69df2db483a52af59938520edd6ffb85aad3
AnoopVL <avlanjekar4@gmail.com>
2025-05-05 19:10:35 +0530

    interview test done

--- app/interview/[interview_id]/start/page.jsx
+++ app/interview/[interview_id]/start/page.jsx
@@ -5,13 +5,15 @@ import { Mic } from "lucide-react";
 import { Phone } from "lucide-react";
 import { Timer } from "lucide-react";
 import Image from "next/image";
-import React, { useContext, useEffect } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import Vapi from "@vapi-ai/web";
 import AlertConfirmation from "./_components/AlertConfirmation";
+import { toast } from "sonner";
 
 function StartInterview() {
   const { interviewInfo, setInterviewInfo } = useContext(InterviewDataContext);
   const vapi = new Vapi(process.env.NEXT_PUBLIC_VAPI_PUBLIC_KEY);
+  const [activeUser, setActiveUser] = useState(false);
 
   useEffect(() => {
     interviewInfo && startCall();
@@ -78,12 +80,29 @@ Key Guidelines:
         ],
       },
     };
-    vapi.start(assistantOptions);
+    // vapi.start(assistantOptions);
   };
 
   const stopInterview = () => {
     vapi.stop();
   };
+
+  vapi.on("call-start", () => {
+    console.log("Call has started.");
+    toast("Interview Started...");
+  });
+  vapi.on("speech-start", () => {
+    console.log("Assistant speech has started.");
+    setActiveUser(false);
+  });
+  vapi.on("speech-end", () => {
+    console.log("Assistant speech has ended.");
+    setActiveUser(true);
+  });
+  vapi.on("call-end", () => {
+    console.log("Call has ended.");
+    toast("Interview Ended...");
+  });
 
   return (
     <div className="p-20 lg:px-48 xl:px-56 bg-gray-200">
@@ -96,21 +115,31 @@ Key Guidelines:
       </h2>
 
       <div className="grid grid-col-1 md:grid-cols-2 gap-7 mt-7">
-        <div className="bg-[#e6f9ee] h-[400px] rounded-lg flex flex-col gap-3 items-center justify-center border-2 border-[#00a63e] shadow-[0_4px_20px_#00a63e33]">
-          <Image
-            src="/gurujiPortrait.png"
-            alt="ai-interviewer"
-            width={100}
-            height={100}
-            className="w-[100px] h-[100px] rounded-full object-cover ring-4 ring-[#00a63e]"
-          />
+        <div className="h-[400px] rounded-lg flex flex-col gap-3 items-center justify-center border-2 border-[#00a63e] shadow-[0_4px_20px_#00a63e33]">
+          <div className="relative">
+            {!activeUser && (
+              <span className="absolute inset-0 rounded-full opacity-75 bg-[#e6f9ee] animate-ping" />
+            )}
+            <Image
+              src="/gurujiPortrait.png"
+              alt="ai-interviewer"
+              width={100}
+              height={100}
+              className="w-[100px] h-[100px] rounded-full object-cover ring-4 ring-[#00a63e]"
+            />
+          </div>
           <h2 className="text-lg">Ai Recruiter</h2>
         </div>
         <div>
-          <div className="bg-[#e6f9ee] h-[400px] rounded-lg flex flex-col gap-3 items-center justify-center border-2 border-[#00a63e] shadow-[0_4px_20px_#00a63e33]">
-            <h2 className="text-4xl bg-primary text-white p-3 rounded-full px-8 py-6">
-              {interviewInfo?.userName[0]}
-            </h2>
+          <div className="h-[400px] rounded-lg flex flex-col gap-3 items-center justify-center border-2 border-[#00a63e] shadow-[0_4px_20px_#00a63e33]">
+            <div className="relative">
+              {activeUser && (
+                <span className="absolute inset-0 rounded-full opacity-75 bg-[#e6f9ee] animate-ping" />
+              )}
+              <h2 className="text-4xl bg-primary text-white p-3 rounded-full px-8 py-6">
+                {interviewInfo?.userName[0]}
+              </h2>
+            </div>
             <h2 className="text-lg">{interviewInfo?.userName}</h2>
           </div>
         </div>
