07acd9ed40606748a5784cbf3cd081730923e997
AnoopVL <avlanjekar4@gmail.com>
2025-05-13 01:19:32 +0530

    scheduled interview page

---
+++ app/(main)/schedule-interview/page.jsx
@@ -0,0 +1,60 @@
+// app/(main)/schedule-interview/page.jsx
+"use client";
+import { useUser } from "@/app/provider";
+import { useState } from "react";
+import { supabase } from "@/services/supabaseClient";
+import React, { useEffect } from "react";
+import { Button } from "@/components/ui/button";
+import { FaVideo } from "react-icons/fa6";
+import InterviewCard from "../dashboard/_components/InterviewCard";
+
+function ScheduleInterview() {
+  const [interviewList, setInterviewList] = useState([]);
+  const { user } = useUser();
+
+  useEffect(() => {
+    user && GetInterviewList();
+  }, [user]);
+  // To-do: 1. fetch only the interviews where feedback is available
+  //        2. Number of candidates who have attended a particular interview
+  const GetInterviewList = async () => {
+    let { data: interviews, error } = await supabase
+      .from("interviews")
+      .select(
+        "jobPosition, duration, interview_id, created_at, interview-feedback(userEmail)"
+      )
+      .eq("userEmail", user?.email)
+      .order("id", { ascending: false });
+    console.log(interviews);
+    setInterviewList(interviews);
+  };
+  return (
+    <div className="p-5">
+      <h2 className="text-xl font-bold">
+        Interview list with candidate feedback
+      </h2>
+      {interviewList?.length == 0 && (
+        <div className="flex flex-col p-5 space-y-3 items-center">
+          <div className="w-12 h-12 bg-green-100 rounded-lg inline-flex items-center justify-center">
+            <FaVideo className="text-green-700 w-6 h-6" />
+          </div>
+          <h2>You don't have any interviews created</h2>
+          <Button>+ Create New Interview</Button>
+        </div>
+      )}
+      {interviewList && (
+        <div className="grid grid-cols-2 xl:grid-cols-3 gap-5 p-5">
+          {interviewList?.map((interview, index) => (
+            <InterviewCard
+              interview={interview}
+              key={index}
+              viewDetail={true}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ScheduleInterview;
