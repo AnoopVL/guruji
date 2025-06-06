94838bf0a2cc2c2cdd7bbe6670240e5ebb573462
AnoopVL <avlanjekar4@gmail.com>
2025-06-07 01:18:21 +0530

    signOut Alert confirmation

--- app/(main)/dashboard/_components/UserMenu.jsx
+++ app/(main)/dashboard/_components/UserMenu.jsx
@@ -1,5 +1,7 @@
-import { useRouter } from "next/navigation";
+// app/(main)/dashboard/_components/UserMenu.jsx
+import { useState } from "react";
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 import { useUser } from "@/app/provider";
 import { supabase } from "@/services/supabaseClient";
 import {
@@ -10,35 +12,47 @@ import {
   DropdownMenuSeparator,
   DropdownMenuItem,
 } from "@/components/ui/dropdown-menu";
+import SignOutAlertConfirmation from "./SignOutAlertConfirmation";
 
 export default function UserMenu() {
   const { user } = useUser();
   const router = useRouter();
+  const [showSignOutDialog, setShowSignOutDialog] = useState(false);
 
   const handleSignOut = async () => {
     await supabase.auth.signOut();
     router.replace("/");
   };
+
   if (!user) return null;
 
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Image
-          src={user.picture}
-          alt={user.name}
-          width={40}
-          height={40}
-          className="rounded-full cursor-pointer"
-        />
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-48">
-        <DropdownMenuLabel>My Account</DropdownMenuLabel>
-        <DropdownMenuSeparator />
-        {/* <DropdownMenuItem onClick={() => router.push("/dashboard/profile")}> */}
-        <DropdownMenuItem>Profile</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleSignOut}>Sign Out</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Image
+            src={user.picture}
+            alt={user.name}
+            width={40}
+            height={40}
+            className="rounded-full cursor-pointer"
+          />
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-48">
+          <DropdownMenuLabel>{user?.name}</DropdownMenuLabel>
+          <DropdownMenuSeparator />
+          <DropdownMenuItem>Profile</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => setShowSignOutDialog(true)}>
+            Sign Out
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+
+      <SignOutAlertConfirmation
+        open={showSignOutDialog}
+        onOpenChange={setShowSignOutDialog}
+        handleSignOut={handleSignOut}
+      />
+    </>
   );
 }
